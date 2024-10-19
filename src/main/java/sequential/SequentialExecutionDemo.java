c35459d4514b9448e930300a969e147200cd72cd
SomaBirajdar <somabiraj111@gmail.com>
2024-10-19 15:00:06 +0530

    sequential execution demo

---
+++ src/main/java/sequential/SequentialExecutionDemo.java
@@ -0,0 +1,19 @@
+package sequential;
+
+public class SequentialExecutionDemo {
+    public static void main(String[] args) {
+        demo1();
+        demo2();
+    }
+    private static void demo1(){
+        for(int i=0; i<5; i++){
+            System.out.println("From demo 1 "+i);
+        }
+    }
+
+    private static void demo2(){
+        for(int i=5; i<10; i++){
+            System.out.println("From demo 2 "+i);
+        }
+    }
+}
